067ea663a9ef499c912198b032e607ebc89a02af
ckaiser258 <kaiserc2@seattleu.edu>
2020-06-12 17:24:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const a = function(drivers) {
-    return drivers[0]
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0], drivers[1]]
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return [drivers[drivers.length - 2], drivers[drivers.length - 1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function(fare) {
+        return fare * int
+    }
+}
+
+const fareDoubler = function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function fareDoubler(fare) {
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(drivers, func) {
+    if (func === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    }
+    else if (func === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers)
+    }
 }
